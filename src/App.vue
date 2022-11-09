270dbd98239bd9e6d0dcaf36c226ebae057ca575
unknown <gonzalesbenedick@gmail.com>
2022-11-09 14:57:08 +0800

    status

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div> Hello {{ person.name }} </div>
+    <div> My age is {{ person.age }} </div>
+    <div v-if ="person.isDev"> I am dev </div>
+    <div v-else> I am not a dev</div>
+</template>
+
+<script>
+//   // console.log('hello')
+// const myVariable = 'hello'
+  export default{
+      data() {
+        return {
+          person: {
+            name: 'Ben',
+            age: 33,
+            isDev: false,
+         }
+        }
+      }
+}
+</script>
